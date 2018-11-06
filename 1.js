1355266bbaa14490407b6c35818992c2e0f15eb0
trunglk16 <trungnguyenlk16@gmail.com>
2018-11-06 15:57:04 +0700

    first

---
+++ 1.js
@@ -0,0 +1,43 @@
+function upArrowPressed() {
+    var element = document.getElementById("nobita");
+    element.style.top = parseInt(element.style.top) - 5 + "px";
+
+}
+function downArrowPressed() {
+    var element = document.getElementById("nobita");
+    element.style.top = parseInt(element.style.top) + 5 + "px";
+
+}
+function leftArrowPressed() {
+    var element = document.getElementById("nobita");
+    element.style.left = parseInt(element.style.left) - 5 + "px";
+
+}
+function rightArrowPressed() {
+    var element = document.getElementById("nobita");
+    element.style.left = parseInt(element.style.left) + 5 + "px";
+
+}
+
+function docReady()
+{
+    window.addEventListener('keydown', moveSelection);
+}
+function moveSelection(evt) {
+    switch (evt.keyCode) {
+        case 37:
+            leftArrowPressed();
+            break;
+        case 39:
+            rightArrowPressed();
+            break;
+        case 38:
+            upArrowPressed();
+            break;
+        case 40:
+            downArrowPressed();
+            break;
+    }
+}
+
+
